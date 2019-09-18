487e0b62ef1099fbd2b26e42dbba72219819efc3
Christian Ewald <vfr750rc24-github@yahoo.com>
2019-09-18 11:08:05 +0200

    Temporarily add button to sign out

--- source/rolx-client/src/app/sign-in/sign-in.component.ts
+++ source/rolx-client/src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, NgZone } from '@angular/core';
 
 
 @Component({
@@ -8,7 +8,7 @@ import { AfterViewInit, Component } from '@angular/core';
 })
 export class SignInComponent implements AfterViewInit {
 
-  constructor() { }
+  constructor(private zone: NgZone) { }
 
   ngAfterViewInit() {
     gapi.signin2.render('sign-in-button', {
@@ -16,12 +16,17 @@ export class SignInComponent implements AfterViewInit {
       width: 240,
       height: 50,
       longtitle: true,
-      theme: 'light',
-      onsuccess: param => this.onSuccess(param) // TODO: call this in the zone
+      theme: 'dark',
+      onsuccess: param => this.zone.run(() => this.onSuccess(param))
     });
   }
 
-  onSuccess(googleUser) {
+  signOut() {
+    const auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(() => console.log('User signed out.'));
+  }
+
+  private onSuccess(googleUser) {
     console.log(googleUser.getBasicProfile());
   }
 
